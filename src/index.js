5ee2d547ddbaf5582360ca7f839c0cb113dce0e0
Daniel DeGuire <danjdeguire@gmail.com>
2022-03-23 19:48:28 -0500

    React Props

--- src/index.js
+++ src/index.js
@@ -4,42 +4,29 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
-// JSX rules
-// Return single element
-// div/ section/ article or Fragment
-// use cameCase for html attribute
-// className instead of class
-// close every element
-// formatting
-
-//stateless functional component
-// always return JSX
-
-// Nested Components, React TOols
-
+// set-up vars
+const title = "I Love You to the Moon and Back";
+const author = "Amelia Hepworth";
+const img =
+  "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg";
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book job="developer" />
+      <Book title="random title" number={22} />
     </section>
   );
 }
 
-const Book = () => {
-  const title = "I Love You to the Moon and Back";
-  const author = "Amelia Hepworth";
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="box">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg"
-        width="300"
-        height="250"
-        alt=""
-      />
+      <img src={img} width="300" height="250" alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {/* <p>{let x=6}</p> */}
-      <p>{6 + 6}</p>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
     </article>
   );
 };
