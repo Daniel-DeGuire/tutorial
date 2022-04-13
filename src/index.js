7126de1761289283e76ed96ef00bfe1d4278f5b0
Daniel DeGuire <danjdeguire@gmail.com>
2022-04-13 18:41:13 -0500

    update to include child

--- src/index.js
+++ src/index.js
@@ -5,28 +5,43 @@ import ReactDom from "react-dom";
 import "./index.css";
 
 // set-up vars
-const title = "I Love You to the Moon and Back";
-const author = "Amelia Hepworth";
-const img =
-  "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg";
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81Jg8HF-N1L._AC_UL604_SR604,400_.jpg",
+  title: "How to Catch the Easter Bunny",
+  author: "Adam Wallace",
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book job="developer" />
-      <Book title="random title" number={22} />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>lorem Ipsum</p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = (props) => {
-  console.log(props);
+const Book = ({ img, title, author, children }) => {
   return (
     <article className="box">
       <img src={img} width="300" height="250" alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      <p>{props.job}</p>
-      <p>{props.title}</p>
+      {children}
     </article>
   );
 };
