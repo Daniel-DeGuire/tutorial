074f182eb15a54423cf25ed18ea9b212f3b06d6d
Daniel DeGuire <danjdeguire@gmail.com>
2022-04-13 19:31:32 -0500

    Add ID to variables

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import "./index.css";
 // set-up vars
 const books = [
   {
+    id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg",
     title: "I Love You to the Moon and Back",
     author: "Amelia Hepworth",
   },
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/81Jg8HF-N1L._AC_UL604_SR604,400_.jpg",
     title: "How to Catch the Easter Bunny",
     author: "Adam Wallace",
   },
   {
+    id: 3,
     img: "https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL604_SR604,400_.jpg",
     title: "It's Not Easy Being a Bunny",
     author: "Marilyn Sadlere",
@@ -26,7 +29,7 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book book={book}></Book>;
+        return <Book key={book.id} book={book}></Book>;
       })}
     </section>
   );
